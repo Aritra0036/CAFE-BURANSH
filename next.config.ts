1d0202e460e2fe67aaf858b6e2f0d8cd58da8925
Aritra0036 <aritra0036@gmail.com>
2025-04-06 21:08:02 +0530

    Configure static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/CAFE-BURANSH",     // 👈 Replace with actual repo name
+  assetPrefix: "/CAFE-BURANSH",
 };
 
 export default nextConfig;
